5e39a3cdec8d3fe0957ac63858b81fb397870fc6
Marco <130693826+MarcoSchenker@users.noreply.github.com>
2025-10-25 12:52:11 -0300

    Revert "Feat new"

--- src/test/kotlin/org/gudelker/authorization/AuthorizationApplicationTests.kt
+++ src/test/kotlin/org/gudelker/authorization/AuthorizationApplicationTests.kt
@@ -1,12 +1,11 @@
 package org.gudelker.authorization
 
-import org.gudelker.authorization.security.AuthenticationServiceApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
-@SpringBootTest(classes = [AuthenticationServiceApplication::class])
+@SpringBootTest
 class AuthorizationApplicationTests {
     @Test
     fun contextLoads() {
