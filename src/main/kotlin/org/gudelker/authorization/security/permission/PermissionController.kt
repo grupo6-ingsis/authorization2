75461c9a57fb6239586533a779b92b46fb25e4dc
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-10-30 17:15:48 -0300

    .

--- src/main/kotlin/org/gudelker/authorization/security/permission/PermissionController.kt
+++ src/main/kotlin/org/gudelker/authorization/security/permission/PermissionController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -31,7 +32,7 @@ class PermissionController(private val permissionService: PermissionService) {
     @GetMapping("/{snippetId}")
     fun getPermissionsForSnippet(
         @PathVariable snippetId: String,
-        userId: String,
+        @RequestParam userId: String
     ): List<PermissionType> {
         return permissionService.getPermissionsForSnippet(userId, snippetId)
     }
