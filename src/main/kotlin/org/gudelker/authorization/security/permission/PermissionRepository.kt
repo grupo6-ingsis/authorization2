4bd1a12f086003468aa57896f3a1f8ab6abc50e8
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-02 13:11:00 -0300

    fix: uuid in repository generation id

--- src/main/kotlin/org/gudelker/authorization/security/permission/PermissionRepository.kt
+++ src/main/kotlin/org/gudelker/authorization/security/permission/PermissionRepository.kt
@@ -2,9 +2,10 @@ package org.gudelker.authorization.security.permission
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface PermissionRepository : JpaRepository<Permission, Long> {
+interface PermissionRepository : JpaRepository<Permission, UUID> {
     fun findByUserIdAndSnippetId(
         userId: String,
         snippetId: String,
